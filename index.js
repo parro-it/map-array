780c67090e807597a655d5e1f32658cdef3ef71c
Andrea Parodi <andrea@parro.it>
2017-03-02 16:54:52 +0100

    ♥️  + 🚀

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 const map = require('map-obj');
 
-module.exports = function mapToArray(obj, fn) {
-  let idx = 0;
-  const result = map(obj, (key, value) =>
-    [idx++, fn(key, value)]
-  );
-  result.length = idx;
-  return Array.from(result);
-};
+function mapToArray(obj, fn) {
+	let idx = 0;
+	const result = map(obj, (key, value) =>
+		[idx++, fn(key, value)]
+	);
+	result.length = idx;
+	return Array.from(result);
+}
+
+module.exports = mapToArray;
--- test.js
+++ test.js
@@ -1,12 +1,11 @@
-const test = require('tape');
-const mapArray = require('./');
+import test from 'ava';
+import mapArray from './';
 
 test('convert an object to array', t => {
-  const obj = {
-    name: 'Giorgio',
-    surname: 'Bianchi'
-  };
-  const result = mapArray(obj, (key, value) => key + ' ' + value);
-  t.deepEqual(result, ['name Giorgio', 'surname Bianchi']);
-  t.end();
+	const obj = {
+		name: 'Giorgio',
+		surname: 'Bianchi'
+	};
+	const result = mapArray(obj, (key, value) => key + ' ' + value);
+	t.deepEqual(result, ['name Giorgio', 'surname Bianchi']);
 });
